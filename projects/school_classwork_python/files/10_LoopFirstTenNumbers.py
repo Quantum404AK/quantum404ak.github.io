7aaaefc58a9adc855a504d9eb2ffeb813110d989
Akshaj Khurana <khurana.akshajt@gmail.com>
2026-03-10 22:03:37 +0530

    Delete projects/school_classwork_python directory

--- projects/school_classwork_python/files/10_LoopFirstTenNumbers.py
+++
@@ -1,7 +0,0 @@
-num = 1
-print ("First ten natural numbers")
-print("---------------------------")
-while num<=10:
-    print (num)
-    num+=1
-print ("Loop over...")
--- projects/school_classwork_python/files/12_MultiplicationTable.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter the number for multiplication table: "))
-print("-------------------------------------------------------")
-print()
-
-i = 1
-
-while i <=10:
-    print(a, "×", i, "\t=", (a*i))
-    i+=1
